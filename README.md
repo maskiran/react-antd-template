2fa1661c4bd11b1a8150dc6562e6ecfd8952db0c
Kiran Madabhushi <maskiran@gmail.com>
2023-03-14 16:23:44 -0700

    delete main branch and make the branches corresponding to the tools/framework/libraries used

---
+++ README.md
@@ -0,0 +1,7 @@
+Checkout the branch corresponding to the tools/framework/library
+versions
+
+## Available branches:
+
+* antd4-react17 (uses create-react-app)
+
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import App from './App-Global-Sidebar'
-import App from './App-CmpSpecific-Sidebar'
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
